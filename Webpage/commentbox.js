95f492ee14c1e233cee8400790b2b7882ec2679f
Marlente <marlo-k@gmx.de>
2022-07-25 12:15:06 +0800

    expanded comment box by name and email

--- Webpage/commentbox.js
+++ Webpage/commentbox.js
@@ -1,10 +1,17 @@
 var post= document.getElementById("post");
 post.addEventListener("click", function(){
-    var commentBoxValue= document.getElementById("comment-box").value;
+    var commentBoxValue= document.getElementById("comment-box-name").value + ", " + document.getElementById("comment-box-email").value + ":";
  
-    var li = document.createElement("li");
-    var text = document.createTextNode(commentBoxValue);
-    li.appendChild(text);
-    document.getElementById("unordered").appendChild(li);
+    var li1 = document.createElement("li");
+    var text1 = document.createTextNode(commentBoxValue);
+    li1.appendChild(text1);
+    document.getElementById("unordered").appendChild(li1);
+
+    var commentBoxComment= document.getElementById("comment-box-comment").value;
+ 
+    var li2 = document.createElement("li");
+    var text2 = document.createTextNode(commentBoxComment);
+    li2.appendChild(text2);
+    document.getElementById("unordered").appendChild(li2);
  
 });
