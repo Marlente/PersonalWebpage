7c7da915f54478e478c83faf93f5c76150e52f67
Marlente <marlo-k@gmx.de>
2022-07-25 12:02:38 +0800

    added redirection link in index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './Webpage/**/*.{html, js, ts, vue}', 
+    './Webpage/**/*.{html, js, ts, vue}', './Projects/**/*.{html, js, ts, vue}',
     './Webpage/**/*', './Projects/**/*', './Blog Posts/**/*'],
   theme: {
     extend: {
